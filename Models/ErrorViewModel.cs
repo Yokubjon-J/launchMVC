14700548d801acaf9b41ab591f185b47c23edcab
U1811042 <U1811042@CZC43518KF>
2023-05-28 03:26:42 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace launchMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
